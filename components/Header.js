5fbd7fd3f0d18b7481877cfb3b56dcdd7622faa4
leandro <0xleanlp@gmail.com>
2023-03-01 10:26:54 -0300

    fix

--- components/Header.js
+++ components/Header.js
@@ -19,9 +19,9 @@ const Header = () => {
   useEffect(() => {
     setTokenBalComp(
       <>
-        <TokenBalance name={'CoinA'} walletAddress={address} />
-        <TokenBalance name={'CoinB'} walletAddress={address} />
-        <TokenBalance name={'CoinC'} walletAddress={address} />
+        {/* <TokenBalance name={'CoinA'} walletAddress={address} /> */}
+        {/* <TokenBalance name={'CoinB'} walletAddress={address} /> */}
+        {/* <TokenBalance name={'CoinC'} walletAddress={address} /> */}
       </>,
     )
 
