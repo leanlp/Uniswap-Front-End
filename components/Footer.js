278d6acc1e33a57df7f925dfd3304ea38ce7980e
leandro <0xleanlp@gmail.com>
2023-03-01 13:38:47 -0300

    swapper

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 import TokenBalance from './TokenBalance'
+import { useAccount } from 'wagmi'
+
+
 
 const Footer = () => {
+
+  const [tokenBalComp, setTokenBalComp] = useState()
+
+  const { address } = useAccount()
+  
+
   return (
     <div className='flex fixed bottom-4 left-1/2 -translate-x-1/2'>
-      <TokenBalance name={'0xLean'} />
-      <TokenBalance name={'0xBuild'} />
-      <TokenBalance name={'0xCLS'} />
+      <TokenBalance name={'0xLean'} walletAddress={address}/>
+      <TokenBalance name={'0xBuild'} walletAddress={address} />
+     <TokenBalance name={'0xCLS'} walletAddress={address} />
     </div>
   )
 }
